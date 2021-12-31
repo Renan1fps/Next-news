051e1b66b6b505d35037c3af34a03a04959975ce
Renan <renan.sergipe68@gmail.com>
2021-12-31 17:24:16 -0300

    feat: add sass to the project

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
-
+import style from '../styles/home.module.scss'
 export default function Home() {
   return (
-    <h1>Base project</h1>
+    <div className={style.container}>
+      <h1 >Base project</h1>
+    </div>
+    
   )
 }
