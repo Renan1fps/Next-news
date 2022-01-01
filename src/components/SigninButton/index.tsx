f1c1e9d928bdc100d5a731e1c9ee10bd477040d5
Renan <renan.sergipe68@gmail.com>
2022-01-01 19:34:44 -0300

    refactor: authentication logic

--- src/components/SigninButton/index.tsx
+++ src/components/SigninButton/index.tsx
@@ -1,9 +1,19 @@
 import style from './style.module.scss'
 import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi'
+import { useState } from 'react'
+
 export function SigninButton() {
-  return (
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+  return isAuthenticated ? (
     <button className={style.signinButton}>
-      <FaGithub />
+      <FaGithub color='#84d361' />
+      Renan1fps
+      <FiX color='#737380' className={style.closeIcon} onClick={() => setIsAuthenticated(false)} />
+    </button>
+  ) : (
+    <button className={style.signinButton}>
+      <FaGithub color='#eba417' />
       Realixar login
     </button>
   )
