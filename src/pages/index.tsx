226c4c0f0d453db274264276b6bee66bdcfda311
Renan <renan.sergipe68@gmail.com>
2022-01-01 20:50:37 -0300

    feat: component subscribeButton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import style from '../styles/pagesStyle/home.module.scss'
 import Head from 'next/head'
+import { SubscribeButton } from '../components/SubscribeButton'
 export default function Home() {
   return (
     <>
@@ -14,8 +15,9 @@ export default function Home() {
             Get access to all publications <br />
             <span>for $9.90 month</span>
           </p>
+          <SubscribeButton />
         </section>
-        <img src='/images/avatar.svg' alt='Girl coding' width='250px'/>
+        <img src='/images/avatar.svg' alt='Girl coding' width='250px' />
       </main>
     </>
   )
