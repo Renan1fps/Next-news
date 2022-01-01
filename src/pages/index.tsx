1049b5d744f0a81d33b3a575467e5dbc6d40d456
Renan <renan.sergipe68@gmail.com>
2022-01-01 20:35:40 -0300

    feat: style home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import style from '../styles/pagesStyle/home.module.scss'
 import Head from 'next/head'
 export default function Home() {
   return (
@@ -5,16 +6,16 @@ export default function Home() {
       <Head>
         <title>Ig news | Início</title>
       </Head>
-      <main>
-        <section>
+      <main className={style.contentContainer}>
+        <section className={style.hero}>
           <span>👋 Hello, welcome</span>
-          <h1>New about <span>React</span> world.</h1>
+          <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all publications <br />
             <span>for $9.90 month</span>
           </p>
         </section>
-        <img src='/images/avatar.svg' alt='Girl coding' />
+        <img src='/images/avatar.svg' alt='Girl coding' width='250px'/>
       </main>
     </>
   )
