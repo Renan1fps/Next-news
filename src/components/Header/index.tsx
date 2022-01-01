910a045fb53b8255ae0437c808ec8502d41e7e52
Renan <renan.sergipe68@gmail.com>
2022-01-01 18:46:57 -0300

    feat: styleheader

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,11 @@
+import style from './style.module.scss'
 export function Header(){
   return (
-    <header>
-      <div>
+    <header className={style.headerContainer}>
+      <div className={style.headerContent}>
         <img src="/images/logo.svg" alt="ig.news logo"/>
         <nav>
-          <a>Home</a>
+          <a className={style.active}>Home</a>
           <a>Posts</a>
         </nav>
       </div>
