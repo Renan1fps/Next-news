b6b5fbd3816261d2c201525d7d8ca52d5dbbcf7e
Renan <renan.sergipe68@gmail.com>
2022-01-03 19:41:37 -0300

    refactor: change home to static generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import style from '../styles/pagesStyle/home.module.scss'
 import Head from 'next/head'
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import { SubscribeButton } from '../components/SubscribeButton'
 import { stripe } from '../services/stripe'
 
@@ -33,7 +33,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1KDnlWBeu0CCKros9ZBW0p0O', {
     expand: ['product']
   });
@@ -48,6 +48,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 horas para revalidar
   }
 }
